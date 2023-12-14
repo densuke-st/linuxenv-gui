d0699f04b821b8462dd101fd0e2556e6c9403a01
SATO Daisuke <densuke@st.kobedenshi.ac.jp>
2023-12-14 10:41:59 +0900

    [add] Docker環境チェックコード

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,30 @@
+use bollard_next::Docker;
+
+/// dockerの環境にアクセスできるか(ローカル限定)をチェックする
+///
+/// # Returns
+/// bool: true: dockerの環境にアクセスできる
+///
+/// # Examples
+/// ```
+/// use src_tauri::docker::check_docker_env;
+/// ```
+async fn check_docker_env() -> bool {
+    let docker = Docker::connect_with_local_defaults().unwrap();
+    let info = docker.info().await;
+    info.is_ok()
+}
+
+
+
+/// check_docker_env()のテストコード
+/// Docker環境を立ち上げていればtrueです、失敗を見たければdocker engineを停止してください
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_check_docker_env() {
+        assert_eq!(check_docker_env().await, true);
+    }
+}
